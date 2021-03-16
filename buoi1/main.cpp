857366d6f71b4f91539a4427fb34cbbe9363254a
tomnyson <tabletkindfire@gmail.com>
2021-03-16 10:33:41 +0700

    show final

---
+++ buoi1/main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+// #include <stdio.h>
+// khai báo thư viện c++
+// usingname std có nghĩa là khai báo thư viện nhập xuất
+// cout in ra màn hình console
+// bắt buộc phải có một hàm man để chạy chương trình
+int main() {
+  cout << "hello world nhap mon lap trinh";
+  return 0;
+}
