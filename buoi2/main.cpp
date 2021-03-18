5fc0f29e56adb5c98bda8cd1f0d2d9aacc5d4556
tomnyson <tabletkindfire@gmail.com>
2021-03-18 11:19:45 +0700

    buoi 2 cach khai bao va su dung bien

---
+++ buoi2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+const float PI = 3.14;
+int main() {
+    // int toan = 8;
+    // int ly = 5;
+    // int hoa = 5;
+    // // tinh diem trung binh double| float
+    // float dtd = (toan + ly + hoa)/3;
+    // cout << "DTB: " <<  dtd;
+    // phép nhân *
+    // int tuoi;
+    // cout << "moi ban nhap tuoi cua ban" << endl;
+    // cin >> tuoi;
+    // cout << "so tuoi cua ban la" << tuoi << endl;
+    // dien tich  PI * R*R
+    // chuvi: PI*2*R™
+    // bai 1: tinh dien tich va chu vi hinh cnd
+    // bai 2: tinh dt va chuvi hinh tron
+    // bai 3: nhap diem toan ly hoa tinh diem trung binh (toan+ly+hoa)/3
+    // bài 4: tinhdt va chu vi hinh vuong
+    // bai 5: tinh dien tich va chu vi hinh tam giac
+    int tich = 4*2;
+    cout << "tich: " << tich << endl;
+    return 0;
+}
